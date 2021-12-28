06f542fc53c4425eb6df563f6ba236520a8c0929
minung.han <hmu332233@gmail.com>
2021-12-28 15:58:53 +0900

    feat: 기본 Header 및 box 추가

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # tab-title-manager
+
+
+
+### 새로 사용해보려고한 것
+- react router dom v6
--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,18 +1,36 @@
 import React from 'react';
 
-
 function App() {
   return (
-    <section className="section">
-      <div className="container">
-        <h1 className="title">
-          Hello World
-        </h1>
-        <p className="subtitle">
-          My first website with <strong>Bulma</strong>!
-        </p>
-      </div>
-    </section>
+    <>
+      <nav className="navbar" role="navigation" aria-label="main navigation">
+        <div className="container is-max-desktop is-flex is-justify-content-center">
+          <div className="navbar-brand">
+            <a className="navbar-item">
+              <h1 className="title has-text-weight-bold">
+                <span className="has-text-primary">Tab-Title</span> Manager
+              </h1>
+            </a>
+          </div>
+        </div>
+      </nav>
+      <section className="section">
+        <div className="container is-max-desktop">
+          <div className="box">
+            I'm in a box.
+          </div>
+          <div className="box">
+            I'm in a box.
+          </div>
+          <div className="box">
+            I'm in a box.
+          </div>
+          <div className="box">
+            I'm in a box.
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
 
